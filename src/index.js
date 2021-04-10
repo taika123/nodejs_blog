e6377fcb113c14752d371f948c9eaa6677264f32
Doro <taika949@gmail.com>
2021-04-10 18:06:22 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -18,12 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use(morgan('combined'));
 //Template engine
 app.engine(
-    'hbs',
-    handlebars({
-        extname: '.hbs',
+          'hbs',
+        handlebars({
+              extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
+app.set(    'view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //HOME . search . contact
